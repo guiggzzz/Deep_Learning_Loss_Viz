fcc8d65547dbdf2b03ec6fe092b7e7d622808369
Guillaume Reynier <guillaumereynier@outlook.fr>
2026-01-14 17:35:41 +0100

    enlever documents en trop

--- main.py
+++ main.py
@@ -1,21 +1,10 @@
 import subprocess
 import sys
 
-list = False
-
 if __name__ == "__main__":
     config_name = sys.argv[1]
     print(f"Paramètre reçu : {config_name}")
-
-    if list:
-        for config in config_name:
-            subprocess.run(["python", "train.py", config])
-            subprocess.run(["python", "directions.py", config])
-            subprocess.run(["python", "landscape.py", config])
-            subprocess.run(["python", "plot.py", config])
-
-    else:
-        subprocess.run(["python", "train.py", config_name])
-        subprocess.run(["python", "directions.py", config_name])
-        subprocess.run(["python", "landscape.py", config_name])
-        subprocess.run(["python", "plot.py", config_name])
+    subprocess.run(["python", "train.py", config_name])
+    subprocess.run(["python", "directions.py", config_name])
+    subprocess.run(["python", "landscape.py", config_name])
+    subprocess.run(["python", "plot.py", config_name])
