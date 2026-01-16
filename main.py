b17f1189d88972b847a927b2dd19e111b5a26caf
Guillaume Reynier <guillaumereynier@outlook.fr>
2026-01-16 08:37:34 +0100

    main avec all

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
+import os
 import subprocess
 import sys
 
 if __name__ == "__main__":
     config_name = sys.argv[1]
+
+    if config_name =="all":
+        config_names = [
+           os.path.splitext(f)[0] for f in os.listdir("configs")
+           if f.endswith(".yaml") 
+        ]
+    else:
+        config_names = [config_name]
     print(f"Paramètre reçu : {config_name}")
-    subprocess.run(["python", "train.py", config_name])
-    subprocess.run(["python", "directions.py", config_name])
-    subprocess.run(["python", "landscape.py", config_name])
-    subprocess.run(["python", "plot.py", config_name])
+
+    for cfg in config_names:
+        subprocess.run(["python", "train.py", cfg])
+        subprocess.run(["python", "directions.py", cfg])
+        subprocess.run(["python", "landscape.py", cfg])
+        subprocess.run(["python", "plot.py", cfg])
